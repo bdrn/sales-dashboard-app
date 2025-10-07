8b438ff0845cee46b66010e9e67fb2b17adf2441
Mohamad <mohamad.badran@gmail.com>
2025-10-07 16:52:08 +0200

    initial commit with vite setup, prettier and lint

---
+++ eslint.config.js
@@ -0,0 +1,27 @@
+import js from "@eslint/js";
+import globals from "globals";
+import react from "eslint-plugin-react";
+import prettier from "eslint-plugin-prettier";
+import configPrettier from "eslint-config-prettier";
+
+export default [
+  js.configs.recommended,
+
+  react.configs.flat.recommended,
+  {
+    files: ["**/*.{js,jsx,mjs,cjs}"],
+    languageOptions: {
+      ecmaVersion: "latest",
+      sourceType: "module",
+      globals: globals.browser,
+    },
+    plugins: { prettier },
+    settings: {
+      react: { version: "detect" },
+    },
+    rules: {
+      "prettier/prettier": "error",
+    },
+  },
+  configPrettier,
+];
