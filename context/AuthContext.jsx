492f36a0d8315b68744397f2b7594443934ec9f0
Mohamad <mohamad.badran@gmail.com>
2025-10-10 12:06:45 +0200

    session handling

--- context/AuthContext.jsx
+++ context/AuthContext.jsx
@@ -1,12 +1,34 @@
-import { createContext, useState, useContext } from "react";
+import { createContext, useState, useContext, useEffect } from "react";
+import supabase from "../src/supabase-client";
 
 const AuthContext = createContext();
 
 export const AuthContextProvider = ({ children }) => {
-  // Auth functions (signin, signup, logout)
-
-  // Session state (userinfo, signin status)
   const [session, setSession] = useState(undefined);
+
+  useEffect(() => {
+    async function getInitialSession() {
+      try {
+        const { data, error } = await supabase.auth.getSession();
+        if (error) {
+          throw error;
+        }
+
+        console.log(data.session);
+        setSession(data.session);
+      } catch (error) {
+        console.error("Error getting session:", error.message);
+      }
+    }
+    getInitialSession();
+
+    supabase.auth.onAuthStateChange((_event, session) => {
+      setSession(session);
+      console.log("Session changed:", session);
+    });
+  }, []);
+
+  // Auth functions (signin, signup, logout)
 
   return (
     <AuthContext.Provider value={{ session }}>{children}</AuthContext.Provider>
